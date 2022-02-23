b462bf0fce95d71b0dd3eef01704215efd78d4ab
Shaon2225 <nazim1703073@stud.kuet.ac.bd>
2022-02-23 22:02:25 +0600

    before audio play

--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@
 const surahPlayer = document.getElementById('surah-player');
 const surahText = document.getElementById('surah-text');
 const surahContainer = document.getElementById('surah-name-container')
+let surahName
+getData();
 
 // surah ayah link array
 let ayahLinkAudio =[];
 let ayahtext = []
 
 function getData(){
-    fetch('http://api.alquran.cloud/v1/quran/ar.alafasy')
+    fetch('https://api.quran.sutanlab.id/surah')
     .then( res => res.json())
     .then( json =>{
-        showSurahInContainer(json.data.surahs);
+        showSurahInContainer(json.data);
+        surahName = getAyahAndSurahNumber();
     })
 }
 
@@ -19,23 +22,48 @@ function getData(){
 function showSurahInContainer(data){
     for(const x of data){
         surahContainer.innerHTML+=`
-        <div class="d-flex justify-content-between shadow my-2 py-2 px-5 border rounded-pill bg-white">
+        <div id="surah-no" class="d-flex justify-content-between shadow my-2 py-2 px-5 border rounded-pill bg-white">
             <div class="text-left ms-3 mt-4">
                 <img src="asset/img/Islam sign outline 12.png" alt="">
-                <h3 class="fw-semibold fs-5 m-1 z-index-no">${x.number}</h3>
+                <h3 class="fw-semibold fs-5 m-1 z-index-no" >${x.number}</h3>
             </div>
             <div class="text-left">
-                <h2>${x.englishName}</h2>
+                <h2>${x.name.transliteration.en}(${x.name.short})</h2>
                 <p>
-                     Total verse : ${x.ayahs.length} <br>
-                    <small class="my-0">Origin : ${x.revelationType}</small>
+                     Total verse : <span id="total-ayah-no">${x.numberOfVerses}</span> <br>
+                    <small class="my-0">Origin : ${x.revelation.en}</small>
                 </p>
             </div>
          </div>
             `;
     }
 }
-getData();
-document.getElementById('surah-fatiha').addEventListener('click', function (){
-    
-})
+
+
+
+// getting all surah number
+function getAyahAndSurahNumber(){
+    const surahNameArray = document.querySelectorAll('#surah-name-container #surah-no');
+    //  console.log(surahNameArray);
+    surahNameArray.forEach((m,index)=>{
+        m.addEventListener('click',()=>{
+            // playlist push
+            const url = `https://api.quran.sutanlab.id/surah/${index+1}`;
+            fetch(url)
+            .then(res => res.json())
+            .then(json => {
+                let allAyah =json.data.verses;
+                allAyah.forEach((ayah) =>{
+                    ayahLinkAudio.push(ayah.audio.primary);
+                    ayahtext.push(ayah.text.arab);
+                })
+                console.log(ayahLinkAudio);
+                console.log(ayahtext);
+            })
+        })
+    });
+    return surahNameArray;
+}
+
+// audio part 
+
